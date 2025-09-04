ee0e4df0d93bc98218b732293846ff862deacd64
Thu Nguyen <thunguy91@gmail.com>
2025-09-03 22:13:25 -0400

    Practice 2: String Utilities

---
+++ string-utilities/stringUtils.js
@@ -0,0 +1,16 @@
+//Define functions.
+function capitalize(str) {
+    if (!str) return "";
+    return str[0].toUpperCase() + str.slice(1);
+}
+function reverse(str) {
+    if (!str) return "";
+    return str.split("").reverse().join("");
+}
+function contains(str, substr) {
+    if (!str || !substr) return false;
+    return str.includes(substr);
+}
+
+// Export the functions
+module.exports = { capitalize, reverse, contains };
